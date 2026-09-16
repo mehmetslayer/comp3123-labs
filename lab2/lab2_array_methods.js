c3b96a33c1d6eb5610926548f781a18d4e7df3db
mehmetslayer <eminonem@hotmail.com>
2026-09-16 18:50:03 -0400

    lab 2 done

---
+++ lab2/lab2_array_methods.js
@@ -0,0 +1,22 @@
+/*
+Purpose: 
+
+Array methods:
+    forEach((currentElement) => {})
+    map((currentElement) => {})
+    reduce((currentElement) => {})
+    filter((currentElement) => {})
+    search((currentElement) => {})
+
+ */
+
+const courseList = ["COMP3123", "COMP3122", "COMP1230", "FASH1011"]
+courseList.forEach((course) => {
+    console.log(course + "! ")
+})
+
+const filteredCourses = courseList.filter((currentElement) =>{
+    return currentElement[0] === "C"
+})
+
+console.log(filteredCourses)
